1949e41664ed33a9654af054e388e9d5cebc430d
Abbas Ismail <abbas.tel2342@gmail.com>
2019-07-25 01:47:36 +0530

    Linear Regression file added

---
+++ Linear Regression.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 25 00:39:42 2019
+
+@author: Abbas Ismail(abbas.tel2342@gmail.com)
+"""
+import numpy as np
+
+class LinearReg:
+    
+    def __init__(self,lr = 0.05,itr = 1000):
+        self.lr = lr
+        self.itr = itr
+        self.m = 0
+        self.n = 0
+        self.cost_ = []
+        
+    def fit(self,X,Y):
+       self.n = X.shape[0]
+       self.m = X.shape[1]
+       self.w = np.ones((self.m,1))
+       
+       for i in range(self.itr):
+           m = X.shape[1]
+           y_preds = np.dot(X,self.w)
+           self.w = self.w - (self.lr/self.m)*np.dot(X.T,(y_preds-Y))
+           cost = np.sum((y_preds-Y)**2)/(m*2)
+           self.cost_.append(cost)
+           
+           
+    def predict(X):
+       return(np.dot(X,self.w))        
+           
+       
+       
+       
+       
