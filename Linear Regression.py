80f6275d18898ffdd67e7973235c7f884f5d0219
Abbas Ismail <abbas.tel2342@gmail.com>
2019-07-25 14:23:10 +0530

    Logistic Reg. Added

--- Linear Regression.py
+++ Linear Regression.py
@@ -19,18 +19,49 @@ class LinearReg:
        self.n = X.shape[0]
        self.m = X.shape[1]
        self.w = np.ones((self.m,1))
-       
+       self.b = 1
        for i in range(self.itr):
            m = X.shape[1]
-           y_preds = np.dot(X,self.w)
+           y_preds = np.dot(X,self.w)+self.b
            self.w = self.w - (self.lr/self.m)*np.dot(X.T,(y_preds-Y))
-           cost = np.sum((y_preds-Y)**2)/(m*2)
+           self.b = (self.b - (self.lr/self.m)*(y_preds-Y))[0]
+           cost = np.sum((y_preds-Y)**2)
            self.cost_.append(cost)
            
            
-    def predict(X):
-       return(np.dot(X,self.w))        
-           
+    def predict(self,X):
+       return(np.dot(X,self.w)+self.b)      
+      
+        
+class LogisticReg:
+    
+    def sigmoid(X):
+        return (1/(1+np.exp(-X)))
+
+    def __init__(self,lr=0.05,itr = 1000):
+        self.lr = lr
+        self.m = 0
+        self.n = 0
+        self.cost = []
+        self.b = 0
+        self.itr = itr
+
+    def fit(self,X,Y):
+        self.m = X.shape[1]
+        self.n = X.shape[0]
+        
+        self.w  = np.ones((self.m,1))
+        
+        for i in range(itr):
+            y_preds = sigmoid(np.dot(X,self.w)+b)
+            w = w - (self.lr/self.m)*(np.dot(y_preds-Y,X.T))
+            b = b - (self.lr/self.m)*(y_preds-Y)[0]
+            cost = -(1/self.m)*(y_preds*np.log(Y) + (1-y_preds)*np.log(1-Y))
+            self.cost.append(cost)
+            
+    def predict(self,X):
+        return (sigmoid(np.dot(X,self.w)+self.b))
+    
        
        
        
